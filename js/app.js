0c4a749425b8892d64fb5db9f0046c3c1eff0710
Tiago Celestino <tiago.farias@catho.com>
2018-10-04 12:58:32 -0300

    docs: add documentation in the functions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-// Replacing $(document).ready()
+/**
+ * Replacing $(document).ready()
+ * @return void
+ */
 function ready(fn) {
     if (document.readyState != 'loading') {
         fn();
@@ -10,26 +13,56 @@ function ready(fn) {
 // Adicionando função no ready
 ready(onReady);
 
+/**
+ * Carrega funções
+ * @return void
+ */
 function onReady() {
     getPoetry();
     declarePoetry();
 }
 
+/**
+ * Gera um número randomico
+ * @param totalelements - total de poesias
+ * @return number
+ */
 function randomNumber(totalelements) {
     return Math.floor(Math.random() * totalelements);
 }
+
+/**
+ * Adiciona dados no localStorage
+ * @param data - dados a ser salvo no localStorage
+ */
 function setStorage(data) {
     localStorage.setItem('shuffle', JSON.stringify(data));
 }
+
+/**
+ * Retorna todos os dados salvos no localStorage
+ * @return object
+ */
 function getStorage() {
     return JSON.parse(localStorage.getItem('shuffle'));
 }
 
+/**
+ * Retorna uma posição a partir dos dados salvos no localStorage
+ * @param storageData - valores salvos no localStorage
+ * @return number
+ */
 function getIndex(storageData) {
     return storageData.shift() || 0;
 }
 
 let poetry; // global para ser usada no button :/
+
+/**
+ * Define as poesias no localStorage e exibe na tela
+ * @param data - dados a serem salvos no localStorage e exibidos na tela
+ * @return void
+ */
 function setPoetry(data) {
     let storage = getStorage();
 
@@ -56,6 +89,10 @@ function setPoetry(data) {
     setStorage(storage);
 }
 
+/**
+ * Obtem poesias
+ * @return void
+ */
 async function getPoetry() {
     try {
         let response = await fetch('poesias.json');
@@ -72,12 +109,18 @@ async function getPoetry() {
  * Carrega o iframe com a música do youtube
  * @param id - ID do vídeo no youtube
  * @param start - Tempo de início do vídeo
+ * @return void
  */
 function carregarMusica(id, start) {
     var src = 'https://www.youtube.com/embed/' + id + '?loop=1&autoplay=1&start=' + start;
     document.getElementById("musica").src = src;
 }
 
+/**
+ * Exibe a poesia na tela
+ * @param poesia - infos da poesias exibida na tela
+ * @return void
+ */
 function exibirPoesia(poesia) {
     document.getElementById("estrofe").innerText = '"' + poesia.estrofe + '"';
     document.getElementById("poeta").innerText = poesia.poeta;
@@ -85,6 +128,10 @@ function exibirPoesia(poesia) {
     carregarMusica(poesia.id, poesia.start);
 }
 
+/**
+ * Recarrega página com uma no poesia
+ * @return void
+ */
 function declarePoetry() {
     document.querySelector('.declare-button').addEventListener('click', (evt) => {
         evt.preventDefault();
