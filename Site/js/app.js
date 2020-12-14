76ff8f8982ef60f0a93f04e252cc2f2288a7ed34
Igor Rozani <igorrozani@gmail.com>
2020-12-13 22:08:21 -0300

    Link para música no titulo

--- Site/js/app.js
+++ Site/js/app.js
@@ -3,227 +3,221 @@
  * @return void
  */
 function ready(fn) {
-    if (document.readyState != "loading") {
-      fn();
-    } else {
-      document.addEventListener("DOMContentLoaded", fn);
-    }
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
   }
-  
-  ready(onReady);
-  
-  /**
-   * Carrega funções
-   * @return void
-   */
-  function onReady() {
-    if (!location.hash) {
-      getPoetry();
-    } else {
-      getPoetry(window.location.hash.substring(1));
-    }
-  
-    window.addEventListener(
-      "hashchange",
-      (evt) => {
-        if (!location.hash) {
-          getPoetry();
-        } else {
-          getPoetry(window.location.hash.substring(1));
-        }
-      },
-      false
-    );
-  
-    document.querySelector(".declare-button").addEventListener("click", (evt) => {
-      evt.preventDefault();
-      declarePoetry();
-    });
-  
-    document.getElementById("btn-stop").addEventListener("click", (evt) => {
-      evt.preventDefault();
-      stopYoutube();
-    });
-  
-    document.getElementById("btn-play").addEventListener("click", (evt) => {
-      evt.preventDefault();
-      playYoutube();
-    });
-  }
-  
-  /**
-   * Gera um número randomico
-   * @param totalelements - total de poesias
-   * @return number
-   */
-  function randomNumber(totalelements) {
-    return Math.floor(Math.random() * totalelements);
-  }
-  
-  /**
-   * Adiciona dados no localStorage
-   * @param data - dados a ser salvo no localStorage
-   */
-  function setStorage(data) {
-    localStorage.setItem("shuffle", JSON.stringify(data));
-  }
-  
-  /**
-   * Retorna todos os dados salvos no localStorage
-   * @return object
-   */
-  function getStorage() {
-    return JSON.parse(localStorage.getItem("shuffle"));
-  }
-  
-  /**
-   * Retorna uma posição a partir dos dados salvos no localStorage
-   * @param storageData - valores salvos no localStorage
-   * @return number
-   */
-  function getIndex(storageData) {
-    return storageData.shift() || 0;
-  }
-  
-  let poetryCollection, poetry;
-  let isPlayEnabled = true;
-  
-  /**
-   * Define as poesias no localStorage e exibe na tela
-   * @param data - dados a serem salvos no localStorage e exibidos na tela
-   * @param id - hash da música
-   * @return void
-   */
-  function setPoetry(data, id) {
-    let storage = getStorage();
-  
-    poetryCollection = data;
-  
-    if (!storage || !storage.length) {
-      let total = data.length;
-      let shuffle = [];
-  
-      for (let i = 0; i < total; ) {
-        let key = randomNumber(total);
-  
-        if (!shuffle[key]) {
-          shuffle[key] = i;
-          i++;
-        }
-      }
-  
-      storage = shuffle;
-    }
-  
-    let index = id || getIndex(storage);
-    poetry = data[index];
-    exibirPoesia(index);
-    setStorage(storage);
-  }
-  
-  /**
-   * Obtem poesias
-   * @param id - hash da música
-   * @return void
-   */
-  async function getPoetry(id) {
-    try {
-      let response = await fetch("poesias.json");
-      if (response.status === 200) {
-        let data = await response.json();
-        setPoetry(data, id);
-      }
-    } catch (error) {
-      throw new Error(`Erro ao obter dados do JSON: ${error}`);
-    }
-  }
-  
-  /**
-   * Carrega o iframe com a música do youtube
-   * @param youtubeId - Id do vídeo no youtube
-   * @param startTime - Tempo de início do vídeo
-   * @return void
-   */
-  function carregarMusica(youtubeId, startTime) {
-    var src =
-      "https://www.youtube.com/embed/" +
-      youtubeId +
-      "?loop=1&autoplay=1&start=" +
-      startTime;
-    document.getElementById("musica").src = src;
-    document.getElementById("ytb-link").href = src;
-  
-    // Remova os comentários das linhas abaixo para habilitar que nome da música leve para o youtube;
-    //   var ytb_link =
-    //     "https://www.youtube.com/watch?v=" +
-    //     youtubeId +
-    //     "?autoplay=1&start=" +
-    //     startTime;
-      
-  }
-  
-  /**
-   * Exibe a poesia na tela
-   * @param id - Id da música
-   * @return void
-   */
-  function exibirPoesia(id) {
-    location.hash = "#" + id;
-    document.getElementById("estrofe").innerText = `"${poetry.estrofe}"`;
-    document.getElementById("poeta").innerText = `-${poetry.poeta}`;
-    document.getElementById("poesia").innerText = poetry.poesia;
+}
 
-    var conteudo = encodeURIComponent(`"${poetry.estrofe}" - ${poetry.poeta} ${window.location.href}`);
-    document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
-  
-    if (isPlayEnabled)
-      carregarMusica(poetry.youtube.id, poetry.youtube.startTime);
+ready(onReady);
+
+/**
+ * Carrega funções
+ * @return void
+ */
+function onReady() {
+  if (!location.hash) {
+    getPoetry();
+  } else {
+    getPoetry(window.location.hash.substring(1));
   }
-  
-  /**
-   * Recarrega página com uma no poesia
-   * @return void
-   */
-  function declarePoetry() {
-    let storage = getStorage();
-    if (!storage || !storage.length) {
-      window.location.reload();
+
+  window.addEventListener(
+    "hashchange",
+    (evt) => {
+      if (!location.hash) {
+        getPoetry();
+      } else {
+        getPoetry(window.location.hash.substring(1));
+      }
+    },
+    false
+  );
+
+  document.querySelector(".declare-button").addEventListener("click", (evt) => {
+    evt.preventDefault();
+    declarePoetry();
+  });
+
+  document.getElementById("btn-stop").addEventListener("click", (evt) => {
+    evt.preventDefault();
+    stopYoutube();
+  });
+
+  document.getElementById("btn-play").addEventListener("click", (evt) => {
+    evt.preventDefault();
+    playYoutube();
+  });
+}
+
+/**
+ * Gera um número randomico
+ * @param totalelements - total de poesias
+ * @return number
+ */
+function randomNumber(totalelements) {
+  return Math.floor(Math.random() * totalelements);
+}
+
+/**
+ * Adiciona dados no localStorage
+ * @param data - dados a ser salvo no localStorage
+ */
+function setStorage(data) {
+  localStorage.setItem("shuffle", JSON.stringify(data));
+}
+
+/**
+ * Retorna todos os dados salvos no localStorage
+ * @return object
+ */
+function getStorage() {
+  return JSON.parse(localStorage.getItem("shuffle"));
+}
+
+/**
+ * Retorna uma posição a partir dos dados salvos no localStorage
+ * @param storageData - valores salvos no localStorage
+ * @return number
+ */
+function getIndex(storageData) {
+  return storageData.shift() || 0;
+}
+
+let poetryCollection, poetry;
+let isPlayEnabled = true;
+
+/**
+ * Define as poesias no localStorage e exibe na tela
+ * @param data - dados a serem salvos no localStorage e exibidos na tela
+ * @param id - hash da música
+ * @return void
+ */
+function setPoetry(data, id) {
+  let storage = getStorage();
+
+  poetryCollection = data;
+
+  if (!storage || !storage.length) {
+    let total = data.length;
+    let shuffle = [];
+
+    for (let i = 0; i < total;) {
+      let key = randomNumber(total);
+
+      if (!shuffle[key]) {
+        shuffle[key] = i;
+        i++;
+      }
     }
-  
-    let index = getIndex(storage);
-    poetry = poetryCollection[index];
-    exibirPoesia(index);
-  
-    setStorage(storage);
+
+    storage = shuffle;
   }
-  
-  /**
-   * Para de tocar vídeo do youtube
-   * @return void
-   */
-  function stopYoutube() {
-    document.getElementById("musica").src = "";
-  
-    isPlayEnabled = false;
-    VisibilityAudioButtons();
+
+  let index = id || getIndex(storage);
+  poetry = data[index];
+  exibirPoesia(index);
+  setStorage(storage);
+}
+
+/**
+ * Obtem poesias
+ * @param id - hash da música
+ * @return void
+ */
+async function getPoetry(id) {
+  try {
+    let response = await fetch("poesias.json");
+    if (response.status === 200) {
+      let data = await response.json();
+      setPoetry(data, id);
+    }
+  } catch (error) {
+    throw new Error(`Erro ao obter dados do JSON: ${error}`);
   }
-  
-  /**
-   * Inicia o vídeo do youtube
-   * @return void
-   */
-  function playYoutube() {
+}
+
+/**
+ * Carrega o iframe com a música do youtube
+ * @param youtubeId - Id do vídeo no youtube
+ * @param startTime - Tempo de início do vídeo
+ * @return void
+ */
+function carregarMusica(youtubeId, startTime) {
+  const src =
+    "https://www.youtube.com/embed/" +
+    youtubeId +
+    "?loop=1&autoplay=1&start=" +
+    startTime;
+  document.getElementById("musica").src = src;
+
+  const ytb_link = "https://www.youtube.com/watch?v=" + youtubeId;
+  document.getElementById("ytb-link").href = ytb_link;
+}
+
+/**
+ * Exibe a poesia na tela
+ * @param id - Id da música
+ * @return void
+ */
+function exibirPoesia(id) {
+  location.hash = "#" + id;
+  document.getElementById("estrofe").innerText = `"${poetry.estrofe}"`;
+  document.getElementById("poeta").innerText = `-${poetry.poeta}`;
+  document.getElementById("poesia").innerText = poetry.poesia;
+
+  const conteudo = encodeURIComponent(`"${poetry.estrofe}" - ${poetry.poeta} ${window.location.href}`);
+  document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
+
+  if (isPlayEnabled)
     carregarMusica(poetry.youtube.id, poetry.youtube.startTime);
-  
-    isPlayEnabled = true;
-    VisibilityAudioButtons();
+}
+
+/**
+ * Recarrega página com uma no poesia
+ * @return void
+ */
+function declarePoetry() {
+  let storage = getStorage();
+  if (!storage || !storage.length) {
+    window.location.reload();
   }
-  
-  /**
-   * Modifica habilita e desabilita os botões de audio
-   * @return void
-   */
-  function VisibilityAudioButtons() {
-    document.getElementById("btn-stop").disabled = !isPlayEnabled;
-    document.getElementById("btn-play").disabled = isPlayEnabled;
-  }
+
+  let index = getIndex(storage);
+  poetry = poetryCollection[index];
+  exibirPoesia(index);
+
+  setStorage(storage);
+}
+
+/**
+ * Para de tocar vídeo do youtube
+ * @return void
+ */
+function stopYoutube() {
+  document.getElementById("musica").src = "";
+
+  isPlayEnabled = false;
+  VisibilityAudioButtons();
+}
+
+/**
+ * Inicia o vídeo do youtube
+ * @return void
+ */
+function playYoutube() {
+  carregarMusica(poetry.youtube.id, poetry.youtube.startTime);
+
+  isPlayEnabled = true;
+  VisibilityAudioButtons();
+}
+
+/**
+ * Modifica habilita e desabilita os botões de audio
+ * @return void
+ */
+function VisibilityAudioButtons() {
+  document.getElementById("btn-stop").disabled = !isPlayEnabled;
+  document.getElementById("btn-play").disabled = isPlayEnabled;
+}
